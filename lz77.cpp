8d8f8608b4c9b9a02488380066417031fc249b35
Cosmo <150472944+Cosmos255@users.noreply.github.com>
2026-01-24 22:27:06 +0200

    Its painfull

--- lz77.cpp
+++ lz77.cpp
@@ -8,6 +8,7 @@ const int SEARCH_SIZE = 32*1000;
 const int LOOkUP_SIZE = 258;
 const int read_size = SEARCH_SIZE+LOOkUP_SIZE;
 const int min_match = 3;
+const int min_lookup = 10
 
 std::vector<token> tokens;
  
@@ -28,6 +29,8 @@ sooo complicated -_-
 
 */
 
+//I need to swtich to a stuct cuz so many pointer passing is
+//painfiull
 
 
 
@@ -56,20 +59,32 @@ int main(){
     std::string window_slide((SEARCH_SIZE+LOOkUP_SIZE), '\0');
 
     in.read(&window_slide[0], read_size);
-    unsigned long int read = in.gcount();
+    uint64_t read = in.gcount();
 
     uint64_t srch = 0;
     uint64_t look = 1;
-    uint64_t notav = 0;
+    uint64_t notav = read;
 
     std::pair<int, int> match; //distance length
-    int distance = 1;
+    uint64_t distance = 1;
 
     while(look-distance >= srch){
-        find_match();
+        if(look-notav < min_lookup && !in.eof()){ // read == read_size might not be the best idea
+            fill_window(read, distance, notav, srch, window_slide);
+        }
+        find_match(distance, look, notav, match);
+        distance++;
+
+        move_window(distance);
     }
 
+    //So it seems i need some hasing for the speed as checkign each 
+    //charcachter bit by bit is quite sloew like n^2
 
+
+}
+
+void find_match(uint64_t &distance, uint64_t &look, uint64_t &notav, std::string &buff, std::pair<int, int> &match){
 
 }
 
@@ -114,6 +129,8 @@ void move_window(int dist){
 
 }
 
-void fill_window(){
-
+void fill_window(uint64_t &read, uint64_t &distance, uint64_t &notav, uint64_t &srch, std::string &window_slide){
+    int fill_size = (srch*2)-notav;
+    in.read(&window_slide[notav], fill_size);
+    read = in.gcount();
 }
